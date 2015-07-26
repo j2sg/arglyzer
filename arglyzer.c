38fb592f2efee9d43c2d5975758d759731459732
Juan Jose Salazar Garcia <jjslzgc@gmail.com>
2015-07-26 18:05:37 +0200

    Added skeleton to analyze function

--- arglyzer.c
+++ arglyzer.c
@@ -2,5 +2,23 @@
 
 int analyze(int argc, char *argv[], OptionPtr *res)
 {
+    if(argc < 1)
+        return -1;
+
+    char curr_opt;
+
+    while(--argc > 0) {
+        if((*++argv)[0] == '-') {
+            if((*argv)[1] == '-') {
+                // Long Option
+            } else
+                while(curr_opt = *++argv[0]) {
+                    // Options
+                }
+        } else {
+            // Parameter
+        }
+    }
+
     return 0;
 }
