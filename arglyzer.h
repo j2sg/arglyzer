3133d550788b49615483768f6b6f094372ab33ce
Juan Jose Salazar Garcia <jjslzgc@gmail.com>
2015-07-30 17:06:27 +0200

    Created header option.h and moved Option type definition to it

--- arglyzer.h
+++ arglyzer.h
@@ -4,16 +4,7 @@
 #define MAXLEN    32
 #define MAXPARAMS 10
 
-typedef struct OptionType *OptionPtr;
-
-typedef struct OptionType
-{
-    char name;
-    char long_name[MAXLEN];
-    unsigned short nparams;
-    unsigned short found;
-    char *param[MAXPARAMS];
-} Option;
+#include "option.h"
 
 int analyze(int argc, char **argv, OptionPtr *res, char **params);
 static OptionPtr find_option(char opt, OptionPtr *options);
