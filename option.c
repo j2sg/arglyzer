536df88ee718b3a6b2316114f13f7a2b6e396e94
Juan Jose Salazar Garcia <jjslzgc@gmail.com>
2015-08-04 16:54:57 +0200

    Option structure members long_name && param will turn in dynamic variables

--- option.c
+++ option.c
@@ -8,16 +8,27 @@ OptionPtr create_option(char name, char *long_name, unsigned short nparams)
     if(!isalpha(name))
         return NULL;
 
-    if(long_name != NULL && (strlen(long_name) == 0 || strlen(long_name) > MAXLEN))
+    if(long_name != NULL && strlen(long_name) < 3)
         return NULL;
 
     OptionPtr option = (OptionPtr) malloc(sizeof(Option));
 
     option -> name = name;
+
     option -> long_name = (long_name != NULL) ? (char *) malloc(sizeof(char) * (strlen(long_name) + 1)) : NULL;
+    if(long_name != NULL)
+        strcpy(option -> long_name, long_name);
+
     option -> nparams = nparams;
+
     option -> found = 0;
-    memset(option -> param, NULL, MAXPARAMS);
+
+    if(option -> nparams == 0)
+        option -> param = NULL;
+    else {
+        option -> param = (char **) malloc(sizeof(char *) * (option -> nparams + 1));
+        memset(option -> param, NULL, option -> nparams + 1);
+    }
 
     return option;
 }
@@ -29,6 +40,9 @@ int free_option(OptionPtr option)
 
     if(option -> long_name != NULL)
         free(option -> long_name);
+
+    if(option -> param != NULL)
+        free(option -> param);
 
     free(option);
 
--- option.h
+++ option.h
@@ -1,18 +1,15 @@
 #ifndef OPTION_H
 #define OPTION_H
 
-#define MAXLEN    32
-#define MAXPARAMS 10
-
 typedef struct OptionType *OptionPtr;
 
 typedef struct OptionType
 {
     char name;
-    char long_name[MAXLEN + 1];
+    char *long_name;
     unsigned short nparams;
     unsigned short found;
-    char *param[MAXPARAMS];
+    char **param;
 } Option;
 
 OptionPtr create_option(char name, char *long_name, unsigned short nparams);
