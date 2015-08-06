d0f9f93603eb5eabed58d0a6751785e17267905c
Juan Jose Salazar Garcia <jjslzgc@gmail.com>
2015-08-06 18:12:17 +0200

    Modified function analyze which take an Option array and return a Result datatype

--- arglyzer.c
+++ arglyzer.c
@@ -2,14 +2,15 @@
 #include <stdlib.h>
 #include <string.h>
 
-int analyze(int argc, char **argv, OptionPtr *res, char **params)
+ResultPtr analyze(int argc, char **argv, OptionPtr *opts)
 {
     if(argc < 1 || argv == NULL)
-        return -1;
+        return NULL;
 
-    if(res == NULL || params == NULL)
-        return -2;
+    if(opts == NULL)
+        return NULL;
 
+    ResultPtr res = create_result(opts, argc - 1);
     OptionPtr curr_opt = NULL;
     char curr_name;
     int np = 0;
@@ -18,32 +19,32 @@ int analyze(int argc, char **argv, OptionPtr *res, char **params)
         if((*++argv)[0] == '-') {
             if((*argv)[1] != '-') {
                 while(curr_name = *++argv[0]) {
-                    if((curr_opt = find_option(curr_name, res)) == NULL)
-                        return -3;
+                    if((curr_opt = find_option(curr_name, res -> options)) == NULL)
+                        return NULL;
 
                     curr_opt -> found++;
                 }
             } else {
-                if((curr_opt = find_long_option(argv[0], res)) == NULL)
-                    return -3;
+                if((curr_opt = find_long_option(argv[0], res -> options)) == NULL)
+                    return NULL;
 
                 curr_opt -> found++;
             }
         } else {
             if(curr_opt == NULL || (curr_opt != NULL && curr_opt -> nparams == 0)) {
-                if(assign_param(argv[0], params++) < 0)
-                    return -2;
+                if(assign_param(argv[0], res -> params++) < 0)
+                    return NULL;
             } else {
                 if(np < curr_opt -> nparams) {
                     if(assign_param(argv[0], &(curr_opt -> param[np])) < 0)
-                        return -2;
+                        return NULL;
 
                     np++;
                 }
 
                 if(np == curr_opt -> nparams) {
-                    if(assign_param(argv[0], params++) < 0)
-                        return -2;
+                    if(assign_param(argv[0], res -> params++) < 0)
+                        return NULL;
 
                     curr_opt = NULL;
                     np = 0;
@@ -52,7 +53,7 @@ int analyze(int argc, char **argv, OptionPtr *res, char **params)
         }
     }
 
-    return 0;
+    return res;
 }
 
 static OptionPtr find_option(char opt, OptionPtr *options)
--- arglyzer.h
+++ arglyzer.h
@@ -1,9 +1,10 @@
 #ifndef ARGLYZER
 #define ARGLYZER
 
+#include "result.h"
 #include "option.h"
 
-int analyze(int argc, char **argv, OptionPtr *res, char **params);
+ResultPtr analyze(int argc, char **argv, OptionPtr *opts);
 static OptionPtr find_option(char opt, OptionPtr *options);
 static OptionPtr find_long_option(char *opt, OptionPtr *options);
 static int assign_param(char *arg, char **param);
