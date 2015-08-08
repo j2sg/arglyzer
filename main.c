8649e0284a5a027a2dc758c0c9366de3ac82f635
Juan Jose Salazar Garcia <jjslzgc@gmail.com>
2015-08-08 22:45:56 +0200

    Print the execution result and free all options

--- main.c
+++ main.c
@@ -10,12 +10,19 @@ int main(int argc, char *argv[])
     OptionPtr option_b = create_option('b', "--long-optionb", 1);
     OptionPtr option_c = create_option('c', "--long-optionc", 2);
     OptionPtr option_d = create_option('d', "--long-optiond", 3);
-    OptionPtr options[N_OPTIONS] = {option_a, option_b, option_c, option_d};
+    OptionPtr options[N_OPTIONS + 1] = {option_a, option_b, option_c, option_d, NULL};
 
     if ((res = analyze(argc, argv, options)) == NULL) {
         fprintf(stderr, "Error during execution.\n");
         return 1;
     }
+
+    print_result(res);
+
+    free_option(option_a);
+    free_option(option_b);
+    free_option(option_c);
+    free_option(option_d);
 
     free_result(res);
 
